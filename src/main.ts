bbb1c0beff2512479337cceb4e336fad6b35ac9e
Neto <picilianovasconcelos@gmail.com>
2023-08-11 12:24:57 -0300

    modified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { inicialize } from "./database/config";
 import { router } from "./app/routes";
 
@@ -7,11 +8,19 @@ dotenv.config();
 
 inicialize();
 
-const app = express();
-app.use(express.json())
+const app = express(); 
+
+const corsOptions = {
+  origin: ["http://localhost:5173"], 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
 app.use(router);
-
-
 
 app.listen(process.env.PORT, () =>
   console.log("servidor rodando na porta", process.env.PORT)
